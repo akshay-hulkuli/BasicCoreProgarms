9ce9317341e134687e573fe9858c19a7fea93494
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-21 20:00:12 +0530

    alphabetChecker

---
+++ src/com/bridgelabz/coreprograms/VowelOrConsonant.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.coreprograms;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+		char alphabet = 'd';
+		
+		// if alphabet is lowercase then convert it into uppercase
+		
+		if(alphabet >= 'a' && alphabet <= 'z') 
+		{
+			alphabet -= ('a'-'A');
+		}
+		
+		
+		if(alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' || alphabet == 'U') 
+		{
+			System.out.println(" given alphabet is VOWEL");
+			
+		}
+		else 
+		{
+			System.out.println("Given alphabet  is CONSONANT");
+		}
+	}
+
+}
