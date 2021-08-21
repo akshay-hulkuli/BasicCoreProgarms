a877f395c5f6d48eb68a81a4905daf5094941722
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-21 16:46:49 +0530

    CoinFlip program ADDED

---
+++ src/com/bridgelabz/coreprograms/HeadAndTailPercentage.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.coreprograms;
+
+public class HeadAndTailPercentage {
+
+	public static void main(String[] args) {
+		int numberOfFlips = 10;
+		int headsCount = 0;
+		for (int i=0; i< numberOfFlips; i++) 
+		{
+			if(numberOfFlips <= 0) 
+			{
+				System.err.println("Number of Flips must be a positive integer");
+			}
+			else 
+			{
+				if(Math.random() > 0.5)
+				{
+					headsCount++;
+				}
+			}
+		}
+		 int headsPercentage = (headsCount*100)/numberOfFlips;
+		 int tailsPercentage = 100-headsPercentage;
+		 System.out.println("Heads Percentage : "+ headsPercentage);
+		 System.out.println("Tails Percentage : "+ tailsPercentage);
+	}
+
+}
