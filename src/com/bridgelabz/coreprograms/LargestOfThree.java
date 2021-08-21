3d5cfb90de15f60d1afadd1d0cd1543ca1c49a3a
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-21 20:11:41 +0530

    LargestOfThree program is added

---
+++ src/com/bridgelabz/coreprograms/LargestOfThree.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.coreprograms;
+
+public class LargestOfThree {
+
+	public static void main(String[] args) {
+		int number1 = 30;
+		int number2 = 24;
+		int number3 = 42;
+		
+		System.out.println("number1:"+ number1 +"  number2:"+ number2 +"  number3:"+ number3 );
+		
+		if(number1 > number2) 
+		{
+			if(number1 > number3) 
+			{
+				System.out.println("number1 is the largest");
+			}
+			else 
+			{
+				System.out.println("number3 is the largest");
+			}
+		}
+		else
+		{
+			if(number2 > number3)
+			{
+				System.out.println("number2 is the largest");
+			}
+			else
+			{
+				System.out.println("number3 is the largest");
+			}
+		}
+
+	}
+
+}
