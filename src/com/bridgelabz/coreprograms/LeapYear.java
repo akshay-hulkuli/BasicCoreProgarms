46facfe947f35872415726f982cb59857622be43
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-21 16:55:04 +0530

    LeapYear  program ADDED

---
+++ src/com/bridgelabz/coreprograms/LeapYear.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.coreprograms;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		int year = 1996;
+		if(Integer.toString(year).length() != 4) 
+		{
+			System.err.println("Please ensure that year is a 4 digit number");
+		}
+		if((year%4 == 0 && year%100 != 0) || year % 400 == 0)
+		{
+			System.out.println(year +" : This is a leap year");
+		}
+		else
+		{
+			System.out.println(year+" : This is not a leap year");
+		}
+
+	}
+
+}
