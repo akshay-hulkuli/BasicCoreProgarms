d2a5de56f7ffa404c3dd2c464e6b2c17d895ee11
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-21 19:42:47 +0530

    EvenOrOdd program ADDED

---
+++ src/com/bridgelabz/coreprograms/EvenOrOdd.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.coreprograms;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		int number = 70;
+		if(number % 2 == 0) System.out.println(number +" : this is an even number");
+		else System.out.println(number +" : this is an odd number");
+	}
+}
