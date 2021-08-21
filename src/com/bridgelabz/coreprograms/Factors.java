cd857d2a1a242fc2e40e3f32db2cb751d0863201
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-21 19:17:00 +0530

    Factor Program ADDED

---
+++ src/com/bridgelabz/coreprograms/Factors.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.coreprograms;
+
+public class Factors {
+	public static void main(String[] args) {
+		int number = 200;
+		if(number <=1 ) return;
+		
+		for(int i=2; i*i<= number; i++)
+		{
+			while(number % i == 0) 
+			{
+				System.out.print(i+" ");
+				number = number / i;
+			}
+		}
+		
+		if(number > 1) System.out.println(number);
+	}
+
+}
